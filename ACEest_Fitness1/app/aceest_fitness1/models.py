fbbe9314d5b33ae12341d9d8206995a8b87e418c
2024tm93152-droid <2024tm93152@wilp.bits-pilani.ac.in>
2025-11-10 20:52:03 +0530

    Delete ACEest_Fitness1 directory

--- ACEest_Fitness1/app/aceest_fitness1/models.py
+++
@@ -1,8 +0,0 @@
-class Member:
-    def __init__(self, id, name, membership):
-        self.id = id
-        self.name = name
-        self.membership = membership
-
-    def to_dict(self):
-        return {"id": self.id, "name": self.name, "membership": self.membership}
