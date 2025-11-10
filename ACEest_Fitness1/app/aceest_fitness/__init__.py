aeea9b2a545902c275706b72030803be19fcb6a6
Amruthaa V <amruthaa@Amruthaas-MacBook-Air.local>
2025-11-10 18:09:05 +0530

    Adding tests

--- ACEest_Fitness1/app/aceest_fitness/__init__.py
+++ ACEest_Fitness1/app/aceest_fitness/__init__.py
@@ -3,7 +3,7 @@ from .views import main_bp
 
 def create_app(config=None):
     app = Flask(__name__)
-    app.config.from_object('aceest_fitness.config.Config')
+    app.config.from_object('app.aceest_fitness.config.Config')
     if config:
         app.config.update(config)
 
